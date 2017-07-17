6a1038317dd8727cebe98761897269aed992f1e6
Santiago Ferreira <san650@gmail.com>
2017-07-17 20:11:33 -0300

    Hide helpers modules from documentation
    Also, simplify implementation and replace unit tests by doctests

--- lib/phoenix_components/helper.ex
+++ lib/phoenix_components/helper.ex
@@ -1,10 +1,32 @@
 defmodule PhoenixComponents.Helpers do
+  @moduledoc false
+
+  import Phoenix.Naming, only: [camelize: 1]
+
+  @doc """
+  Converts a cammel_case string or atom into a PascalCase atom.
+
+  ## Example
+
+      iex> PhoenixComponents.Helpers.to_pascal_case "dummy"
+      :Dummy
+
+      iex> PhoenixComponents.Helpers.to_pascal_case "dummy_string"
+      :DummyString
+
+      iex> PhoenixComponents.Helpers.to_pascal_case "Dummy"
+      :Dummy
+
+      iex> PhoenixComponents.Helpers.to_pascal_case "DUMMY"
+      :DUMMY
+
+      iex> PhoenixComponents.Helpers.to_pascal_case :dummy_string
+      :DummyString
+  """
   def to_pascal_case(name) do
     name
     |> to_string
-    |> String.split("_")
-    |> Enum.map(&String.capitalize/1)
-    |> Enum.join
+    |> camelize
     |> String.to_atom
   end
 end
