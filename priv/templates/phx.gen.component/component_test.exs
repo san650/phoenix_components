573ca9bb2c5e4e39ad27c8062def53a7848d11a7
Reio Piller <reio@heathmont.net>
2018-10-14 23:08:54 +0300

    generator fix

---
+++ priv/templates/phx.gen.component/component_test.exs
@@ -0,0 +1,17 @@
+defmodule <%= @module %>Test do
+  use ExUnit.Case
+
+  test "renders block" do
+    html = PhoenixComponents.View.component <%= @web_module %>.Components, :<%= @name %> do
+      "Hello, World!"
+    end
+
+    assert raw(html) == "<p>Hello, World!</p>"
+  end
+
+  defp raw({:safe, html}) do
+    html
+    |> to_string
+    |> String.trim
+  end
+end
